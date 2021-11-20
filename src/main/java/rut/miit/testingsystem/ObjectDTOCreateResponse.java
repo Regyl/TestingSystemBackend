608e511a1e5712bad511237d09c9bf94a9db74d8
Regyl <novikovevgeny1@gmail.com>
2021-11-20 16:53:56 +0300

    fix docker-compose

--- src/main/java/rut/miit/testingsystem/ObjectDTOCreateResponse.java
+++ src/main/java/rut/miit/testingsystem/ObjectDTOCreateResponse.java
@@ -1,19 +1,14 @@
 package rut.miit.testingsystem;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public abstract class ObjectDTOCreateResponse {
     private LocalDateTime timestamp;
 
     protected ObjectDTOCreateResponse() {
         this.timestamp=LocalDateTime.now();
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
     }
 }
