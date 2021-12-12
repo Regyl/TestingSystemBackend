aefc04c2b3acd68b11b9e570da66a2e955bb3906
Regyl <novikovevgeny1@gmail.com>
2021-12-12 20:17:42 +0300

    refactoring

--- src/test/java/rut/miit/testingsystem/integration/AbstractIntegrationTest.java
+++ src/test/java/rut/miit/testingsystem/integration/AbstractIntegrationTest.java
@@ -4,24 +4,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.*;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 @AutoConfigureMockMvc
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 abstract class AbstractIntegrationTest {
 
-    @Autowired
-    protected MockMvc mvc;
+    protected static final MediaType JSON = MediaType.APPLICATION_JSON;
 
     protected static PodamFactory podamFactory;
 
     @Autowired
-    protected ObjectMapper mapper;
+    protected MockMvc mvc;
 
-    protected static final MediaType JSON = MediaType.APPLICATION_JSON;
+    @Autowired
+    protected ObjectMapper mapper;
 
     static {
         podamFactory = new PodamFactoryImpl();
