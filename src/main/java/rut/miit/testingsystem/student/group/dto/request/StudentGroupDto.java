04cf56295d1b8069a0b502a9092fbd631650e2d6
Regyl <novikovevgeny1@gmail.com>
2021-12-19 00:00:20 +0300

    refactoring

--- src/main/java/rut/miit/testingsystem/student/group/dto/request/StudentGroupDto.java
+++ src/main/java/rut/miit/testingsystem/student/group/dto/request/StudentGroupDto.java
@@ -1,27 +1,14 @@
 package rut.miit.testingsystem.student.group.dto.request;
 
+import lombok.Data;
+
 import javax.validation.constraints.NotNull;
 
+@Data
 public class StudentGroupDto {
     @NotNull
     private String shortName;
 
     @NotNull
     private Integer number;
-
-    public Integer getNumber() {
-        return number;
-    }
-
-    public void setNumber(Integer number) {
-        this.number = number;
-    }
-
-    public String getShortName() {
-        return shortName;
-    }
-
-    public void setShortName(String shortName) {
-        this.shortName = shortName;
-    }
 }
